cf5a42b4bc25a2cebba92525d17be1fe1cc470a6
Harsh-Sindhav <66241752+Harsh-Sindhav@users.noreply.github.com>
2022-06-04 01:23:52 +0530

    6 ms

---
+++ solutions/692. Top K Frequent Words.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String,Integer> map = new HashMap();
+        List<String> ans = new ArrayList();
+        
+        for(int i = 0;i<words.length;i++){
+            map.put(words[i],map.getOrDefault(words[i],0)+1);
+        }
+        
+            PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
+                 (a,b) -> a.getValue()==b.getValue() ? b.getKey().compareTo(a.getKey()) : a.getValue()-b.getValue()
+        );
+        
+        for(Map.Entry<String, Integer> entry: map.entrySet())
+        {
+            pq.offer(entry);
+            if(pq.size()>k)
+                pq.poll();
+        }
+​
+        while(!pq.isEmpty())
+            ans.add(0, pq.poll().getKey());
+        
+        
+        return ans;
+    
+    }
+}
